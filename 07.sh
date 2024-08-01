d55122b900eb655be0a018a1604c331b15c9282d
mani <chm558328>
2024-08-01 09:39:18 +0530

    l

--- 07.sh
+++ 07.sh
@@ -2,7 +2,7 @@
 
 if [ $NUMBER -gt 100 ]
 then
-   echo "given number is greater than 100"
+   echo "given number $NUMBER is greater than 100"
 else
-   echo "given number is not greater than 100"
+   echo "given number $NUMBER is not greater than 100"
 fi
